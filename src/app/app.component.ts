24fb101371b366adee944559b2df173f48944c73
unknown <crispim.rodrigues.oficial@gmail.com>
2023-08-14 19:04:44 -0300

    crated a component item

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Tarefa } from './tarefa';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'TODOapp';
+
+  arrayDeTarefas: Tarefa[] = [];
+
+  constructor() {
+    this.READ_tarefas();
+  }
+  CREATE_tarefa(descricaoNovaTarefa: string) {
+    var novaTarefa = new Tarefa(descricaoNovaTarefa, false);
+    this.arrayDeTarefas.unshift(novaTarefa);
+  }
+
+  READ_tarefas() {
+    this.arrayDeTarefas = [
+      new Tarefa("Estudar Frameworks WEB", false),
+      new Tarefa("Comer Pizza", false),
+      new Tarefa("Ajudar meus pais", false)
+    ];
+  }
 }
